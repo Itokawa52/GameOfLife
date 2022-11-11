10350f0928be2692772dbce4c3db9e3dd2176f37
khapov <khapov@skbkontur.ru>
2022-11-11 20:55:24 +0500

    test fix

---
+++ Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace GameOfLife;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var currentState = new bool[1, 1] {{true}};
+
+        var nextState = Game.GetNextState(currentState);
+        var expectingState = new bool[1, 1] {{false}};
+
+        Assert.AreEqual(expectingState, nextState);
+        // Альтернативный способ с помощью библиотеки FluentAssertions
+        nextState.Should().BeEquivalentTo(expectingState);
+    }
+}
